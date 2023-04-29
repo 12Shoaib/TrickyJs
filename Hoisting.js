c9f4327e9684a620566a610a6d0045df04b98920
shoaib <shoaibmohd916@gmail.com>
2023-04-30 00:15:36 +0530

    Explaining hoisting in js

---
+++ Hoisting.js
@@ -0,0 +1,26 @@
+//Hoisting : This is js behavior were varaibles and functions are moved to the
+//top of thier respective scopes before code is executed.this means they we can use functions and variables 
+//before declaration, as long as they are declared some where within same scopes.
+
+//Note : hoisting is applied to functions and variables(var,let and const) but 
+//in case of var it will return undefined and where as let and const are in TDZ(Temporal dead zone)
+
+//example 1
+console.log(sum(2,2))
+
+function sum(x,y) {
+    return x+y
+}
+
+//exmaple 2 
+
+console.log(x)
+var x= 10  // as i mentioned in case of var the hoisting will happen and the o/p wil be 'undefiend'
+
+
+//example 3
+console.log(y,z)
+//Here let and const are hoisted but they are in Temporal dead zone thats why they will throw an refernce error
+
+const y = 10
+let z = 11
