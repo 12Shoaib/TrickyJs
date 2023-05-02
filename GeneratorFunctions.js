1af547fd5b202562408d25e7bd30c02fbe2126b7
shoaib <shoaibmohd916@gmail.com>
2023-05-03 01:34:19 +0530

    generatorFunctions

---
+++ GeneratorFunctions.js
@@ -0,0 +1,13 @@
+function* generatoFunction () {
+    let i = 0
+    let a =true
+    while(a){
+        yield i++
+        if(i===10){
+        a=false
+        }
+    }
+}
+const gen = generatoFunction()
+// console.log(gen.next())
+
